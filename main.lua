67da261c6f6149979f45fb0e2ee2867d45c31322
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-08 10:23:32 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ S.Anchored = true
 S.CanCollide = true
 S.Transparency = 0
 S.Position = Vector3.new(-8500, -8000, -8000)
-S.Size = Vector3.new(200, 5, 200)
+S.Size = Vector3.new(2000, 20, 2000)
 S.Parent = workspace
 end
 
